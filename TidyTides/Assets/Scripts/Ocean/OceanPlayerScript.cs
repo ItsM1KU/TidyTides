3fc13220f7134704820e470ff5666af12bef880e
Sanjay <sanjayvarma44433@gmail.com>
2024-08-11 18:47:26 +0530

    Fixed health

--- TidyTides/Assets/Scripts/Ocean/OceanPlayerScript.cs
+++ TidyTides/Assets/Scripts/Ocean/OceanPlayerScript.cs
@@ -10,9 +10,7 @@ public class OceanPlayerScript : MonoBehaviour
     [SerializeField] float moveSpeed = 5.0f;
     [SerializeField] LayerMask BoatLayer;
     [SerializeField] TextMeshProUGUI interactText;
-    [SerializeField] TextMeshProUGUI OxyText;
-    [SerializeField] float oxygenLevel = 20f;
-    [SerializeField] float health = 10f;
+
 
     private OceanMovement oceanMovement;
     private Vector2 movement;
@@ -30,12 +28,6 @@ public class OceanPlayerScript : MonoBehaviour
     private void OnDisable()
     {
         oceanMovement.Disable();
-    }
-
-    private void Start()
-    {
-        StartCoroutine(Oxycoroutine(oxygenLevel));
-        StartCoroutine(healthCoroutine(health));
     }
 
     private void Update()
@@ -62,32 +54,4 @@ public class OceanPlayerScript : MonoBehaviour
         rb.velocity = (movement * moveSpeed);
     }
 
-    IEnumerator Oxycoroutine(float oxy)
-    {
-        //float oxyleft = 20;
-        while(oxy > 0)
-        {
-            oxy -= 0.5f;
-            oxygenLevel -= 0.5f;
-            yield return new WaitForSeconds(3f);
-            //Debug.Log(oxy + " Left");
-            Debug.Log(oxygenLevel);
-            OxyText.text = ("Oxygen Level: " + oxy.ToString() + " / 20");
-        }
-    }
-
-    IEnumerator healthCoroutine(float hp)
-    {
-        float oxyRef = oxygenLevel;
-        if(oxyRef < 0)
-        {
-            hp--;
-            yield return new WaitForSeconds(2f);
-            Debug.Log(health + " health left");  
-        }
-        if (hp < 0)
-        {
-            Debug.Log("You drowned!!");
-        }
-    }
 }
---
+++ TidyTides/Assets/Scripts/Ocean/Ocean_PlayerH0.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Ocean_PlayerH0 : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI OxyText;
+    [SerializeField] TextMeshProUGUI hpText;
+    [SerializeField] float oxygenLevel = 20f;
+    [SerializeField] float healthLevel = 1.0f;
+
+
+    private void Start()
+    {
+        StartCoroutine(Oxycoroutine(oxygenLevel));
+    }
+
+    IEnumerator Oxycoroutine(float oxy)
+    {
+        //float oxyleft = 20;
+        while (oxy > 0)
+        {
+            oxy -= 0.5f;
+            oxygenLevel -= 0.5f;
+            yield return new WaitForSeconds(3f);
+            //Debug.Log(oxy + " Left");
+            //Debug.Log(oxygenLevel);
+            OxyText.text = ("Oxygen Level: " + oxy.ToString() + " / 20");
+        }
+        StartCoroutine(healthRoutine(healthLevel));
+    }
+
+    IEnumerator healthRoutine(float hp)
+    {
+        while (hp > 0)
+        {
+            hp -= 0.5f;
+            //Debug.Log(hp + " left!!");
+            hpText.text = ("Health: " + hp.ToString() + " / 5");
+            yield return new WaitForSeconds(2f);
+        }
+        Debug.Log("Player dies!!");
+    }
+
+
+}
