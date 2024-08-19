ed4979145966a45bac0f4d4a74709240d092e6ec
Sanjay <sanjayvarma44433@gmail.com>
2024-08-19 21:38:24 +0530

    removing waste

--- TidyTides/Assets/Scripts/Ocean/OceanPlayerScript.cs
+++ TidyTides/Assets/Scripts/Ocean/OceanPlayerScript.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
+using Unity.VisualScripting;
+using UnityEngine.EventSystems;
 
 public class OceanPlayerScript : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
     [SerializeField] float moveSpeed = 5.0f;
     [SerializeField] LayerMask BoatLayer;
+    [SerializeField] LayerMask garbageLayer;
     [SerializeField] TextMeshProUGUI interactText;
 
 
@@ -41,6 +44,15 @@ public class OceanPlayerScript : MonoBehaviour
             if (oceanMovement.Player.Interact.IsPressed())
             {
                 Debug.Log("Leaving ocean!!");
+            }
+        }
+        else if(Physics2D.OverlapCircle(rb.position, 0.2f, garbageLayer))
+        {
+            interactText.gameObject.SetActive(true);
+            var garbage = Physics2D.OverlapCircle(rb.position, 0.5f, garbageLayer);
+            if (oceanMovement.Player.Interact.IsPressed())
+            {
+                Destroy(garbage.gameObject);
             }
         }
         else
