54ba92546845043f9acc6426aed971e62cc2abf3
Sanjay <sanjayvarma44433@gmail.com>
2024-09-26 12:29:14 +0530

    Some changes

--- TidyTides/Assets/Scripts/UI/DontDestroy.cs
+++ TidyTides/Assets/Scripts/UI/DontDestroy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DontDestroy : MonoBehaviour
 {
-    private static GameObject[] persistentObjects = new GameObject[2];
+    private static GameObject[] persistentObjects = new GameObject[3];
     public int objectIndex;
 
     // Start is called before the first frame update
